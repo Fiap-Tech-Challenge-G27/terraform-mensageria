277b21fbd46f4806461663464e9a867915184d95
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-08-03 15:42:23 -0300

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -5,5 +5,5 @@ resource "aws_sns_topic" "checkout-topic" {
 resource "aws_sns_topic_subscription" "checkout-subscription" {
   topic_arn = aws_sns_topic.checkout-topic.arn
   protocol  = "http"
-  endpoint  = "http://k8s-default-ingressb-97436f9206-1044475668.us-east-1.elb.amazonaws.com/payment"
+  endpoint  = "http://k8s-default-ingressb-97436f9206-1044475668.us-east-1.elb.amazonaws.com/payment/initiate"
 }
