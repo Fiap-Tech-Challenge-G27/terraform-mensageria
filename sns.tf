01ef58b059a0e7bffb4ae4c3c8ada133c7837bd5
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-08-03 14:51:07 -0300

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -4,6 +4,6 @@ resource "aws_sns_topic" "checkout-topic" {
 
 resource "aws_sns_topic_subscription" "checkout-subscription" {
   topic_arn = aws_sns_topic.checkout-topic.arn
-  protocol  = "https"
-  endpoint  = "${data.terraform_remote_state.api-gateway.outputs.aws_apigatewayv2_api_endpoint}/payment"
+  protocol  = "http"
+  endpoint  = "http://k8s-default-ingressb-97436f9206-1044475668.us-east-1.elb.amazonaws.com/payment"
 }
