97496dc7bb2126e91ada5059a04bb96833224b20
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-08-06 20:06:24 -0300

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -9,17 +9,17 @@ resource "aws_sns_topic" "payment-approved-topic" {
 resource "aws_sns_topic_subscription" "payment-approved-customer-subscription" {
   topic_arn = aws_sns_topic.payment-approved-topic.arn
   protocol  = "http"
-  endpoint  = "http://k8s-default-ingressb-97436f9206-1044475668.us-east-1.elb.amazonaws.com/"
+  endpoint  = "http://k8s-default-ingressb-97436f9206-648559237.us-east-1.elb.amazonaws.com/"
 }
 
 resource "aws_sns_topic_subscription" "payment-approved-production-subscription" {
   topic_arn = aws_sns_topic.payment-approved-topic.arn
   protocol  = "http"
-  endpoint  = "http://k8s-default-ingressb-97436f9206-1044475668.us-east-1.elb.amazonaws.com/orders/payment-confirmation"
+  endpoint  = "http://k8s-default-ingressb-97436f9206-648559237.us-east-1.elb.amazonaws.com/orders/payment-confirmation"
 }
 
 resource "aws_sns_topic_subscription" "checkout-subscription" {
   topic_arn = aws_sns_topic.checkout-topic.arn
   protocol  = "http"
-  endpoint  = "http://k8s-default-ingressb-97436f9206-1044475668.us-east-1.elb.amazonaws.com/payment/initiate"
+  endpoint  = "http://k8s-default-ingressb-97436f9206-648559237.us-east-1.elb.amazonaws.com/payment/initiate"
 }
