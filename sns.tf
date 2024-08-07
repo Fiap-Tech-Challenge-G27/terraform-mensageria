2354a8aa2078d4d32443c0e2fcb32722f1bfcb91
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-08-07 20:16:42 -0300

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -2,24 +2,24 @@ resource "aws_sns_topic" "checkout-topic" {
   name = "checkout-topic"
 }
 
-resource "aws_sns_topic" "payment-approved-topic" {
-  name = "payment-approved-topic"
+resource "aws_sns_topic" "payment-status-topic" {
+  name = "payment-status-topic"
 }
 
-resource "aws_sns_topic_subscription" "payment-approved-customer-subscription" {
-  topic_arn = aws_sns_topic.payment-approved-topic.arn
-  protocol  = "http"
-  endpoint  = "http://k8s-default-ingressb-97436f9206-648559237.us-east-1.elb.amazonaws.com/"
+resource "aws_sns_topic_subscription" "payment-status-customer-subscription" {
+  topic_arn = aws_sns_topic.payment-status-topic.arn
+  protocol  = "https"
+  endpoint  = "https://i8z10k6wma.execute-api.us-east-1.amazonaws.com/lanchonete/customers/notification"
 }
 
-resource "aws_sns_topic_subscription" "payment-approved-production-subscription" {
-  topic_arn = aws_sns_topic.payment-approved-topic.arn
-  protocol  = "http"
-  endpoint  = "http://k8s-default-ingressb-97436f9206-648559237.us-east-1.elb.amazonaws.com/orders/payment-confirmation"
+resource "aws_sns_topic_subscription" "payment-status-production-subscription" {
+  topic_arn = aws_sns_topic.payment-status-topic.arn
+  protocol  = "https"
+  endpoint  = "https://i8z10k6wma.execute-api.us-east-1.amazonaws.com/lanchonete/orders/payment-confirmation"
 }
 
 resource "aws_sns_topic_subscription" "checkout-subscription" {
   topic_arn = aws_sns_topic.checkout-topic.arn
-  protocol  = "http"
-  endpoint  = "http://k8s-default-ingressb-97436f9206-648559237.us-east-1.elb.amazonaws.com/payment/initiate"
+  protocol  = "https"
+  endpoint  = "https://i8z10k6wma.execute-api.us-east-1.amazonaws.com/lanchonete/payment/initiate"
 }
